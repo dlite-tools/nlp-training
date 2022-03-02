9b11f807eb7b24a4d0209ad3810e6cac86b2fbf9
tomasosorio <tomas.s.osorio@gmail.com>
2022-03-02 11:57:16 +0000

    feat: add base processor

---
+++ inference/data_processors/processor.py
@@ -0,0 +1,50 @@
+"""Processor Module."""
+from typing import (
+    Optional,
+    Tuple
+)
+
+from inference.data_processors.transformers.base import (
+    Transformer
+)
+from nlpiper.core import Document
+
+
+class Processor:
+    """Apply pre-processing and post-processing to samples."""
+
+    train = False
+
+    def __init__(
+        self,
+        preprocessing: Tuple[Transformer],
+    ):
+        """Apply preprocessing to samples.
+
+        Parameters
+        ----------
+        preprocessing: Tuple[Transformer]
+            List with transforms to be applied on preprocessing.
+        """
+        self.preprocessing = preprocessing
+
+    def preprocess(self, doc: Document) -> Document:
+        """Apply pre-processing to samples.
+
+        Parameters
+        ----------
+        doc: Document
+            Pydantic document object with all document metadata.
+
+        Returns
+        -------
+         invoice
+            Pydantic document object updated with `preprocess` applied.
+        """
+        for transform in self.preprocessing:
+            if transform._data_aug and not self.train:  # skip data augmentation if not in training mode
+                continue
+
+            doc = transform(doc)
+
+        return doc
