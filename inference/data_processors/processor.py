6fd47306887605ebcb584782da8343274e95c651
tomasosorio <tomas.s.osorio@gmail.com>
2022-03-03 09:31:49 +0000

    feat: vocab

--- inference/data_processors/processor.py
+++ inference/data_processors/processor.py
@@ -1,13 +1,13 @@
 """Processor Module."""
-from typing import (
-    Optional,
-    Tuple
-)
+from typing import Sequence, Iterable, Optional
+
+import torch
+from nlpiper.core import Document
+from torchtext.vocab import build_vocab_from_iterator, Vocab
 
 from inference.data_processors.transformers.base import (
     Transformer
 )
-from nlpiper.core import Document
 
 
 class Processor:
@@ -15,10 +15,7 @@ class Processor:
 
     train = False
 
-    def __init__(
-        self,
-        preprocessing: Tuple[Transformer],
-    ):
+    def __init__(self, preprocessing: Sequence[Transformer], vocab: Optional[Vocab] = None):
         """Apply preprocessing to samples.
 
         Parameters
@@ -27,6 +24,7 @@ class Processor:
             List with transforms to be applied on preprocessing.
         """
         self.preprocessing = preprocessing
+        self._vocab = vocab
 
     def preprocess(self, doc: Document) -> Document:
         """Apply pre-processing to samples.
@@ -48,3 +46,13 @@ class Processor:
             doc = transform(doc)
 
         return doc
+
+    def build_vocab(self, dataset: Iterable):
+        def yield_tokens(dataset: Iterable):
+            for _, text in dataset:
+                yield [token.cleaned for token in self.preprocess(Document(text)).tokens]
+
+        self._vocab = build_vocab_from_iterator(yield_tokens(dataset), specials=["<unk>"])
+
+    def vocab(self, doc: Document):
+        return torch.tensor(self._vocab([token.cleaned for token in doc.tokens]))
