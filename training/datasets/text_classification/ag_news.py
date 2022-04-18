809f90345c430d235e3f3f0c696fe1eafa0b4188
tomasosorio <tomas.s.osorio@gmail.com>
2022-04-18 10:13:50 +0100

    chore: improve training time

--- training/datasets/text_classification/ag_news.py
+++ training/datasets/text_classification/ag_news.py
@@ -87,7 +87,5 @@ class AGNewsDataModule(pl.LightningDataModule):
             doc = self.processor.preprocess(Document(text))
             offsets.append(len(doc.output))
             docs.extend(doc.output)
-
-        # docs = torch.cat(docs)  # torch.nn.utils.rnn.pad_sequence(docs, batch_first=True, padding_value=1)
         offsets = torch.tensor(offsets, dtype=torch.long)[:-1].cumsum(dim=0)
         return torch.tensor(labels, dtype=torch.int64), torch.tensor(docs, dtype=torch.int64), offsets
